59ce35993b4f0c36f528a18037e78ef5a5d3875b
Tadeu do Nascimento <tadeunascimento36@gmail.com>
2020-03-13 07:57:13 -0300

    Sistema de autenticação de usuário, classe(Usuario).

---
+++ Java/Swing/Login/Usuario.java
@@ -0,0 +1,26 @@
+package br.com.loginswing;
+
+
+
+public class Usuario {
+
+	private String nomeUsuario;
+	private String senhaUsuario;
+
+	public String getNomeUsuario() {
+		return nomeUsuario;
+	}
+
+	public void setNomeUsuario(String nomeUsuario) {
+		this.nomeUsuario = nomeUsuario;
+	}
+
+	public String getSenhaUsuario() {
+		return senhaUsuario;
+	}
+
+	public void setSenhaUsuario(String senhaUsuario) {
+		this.senhaUsuario = senhaUsuario;
+	}
+
+}
