6b519f6ba0c13ba2ca2bcfde8c077624d51f1332
Tadeu do Nascimento <tadeunascimento36@gmail.com>
2020-03-13 08:00:27 -0300

    Sistema de autenticação de usuário, classe(Conexão).

---
+++ Java/Swing/Login/Conexao.java
@@ -0,0 +1,8 @@
+
+package br.com.loginswing;
+
+public class Conexao {
+	public static String stringConexao = "jdbc:mysql://localhost:3306/nomeBanco?serverTimezone=UTC";
+	public static String usuario = "usuario_banco";
+	public static String senha = "senha_usuario_banco";
+}
